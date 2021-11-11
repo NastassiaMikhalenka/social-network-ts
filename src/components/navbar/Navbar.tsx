1854bc169fc5258287e6db7287f8b934dae73464
nastassia <anastasiyamihalenko@gmail.com>
2021-11-12 01:51:39 +0300

    NavLink 20

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,23 +1,36 @@
 import React from "react";
+import {NavLink} from "react-router-dom";
 import classes from "./navbar.module.css"
 
 const Navbar = () => {
     return (
         <nav className={classes.nav}>
-            <div className='item'>
-                <a href="/profile">Profile</a>
+            <div className={classes.item}>
+                <NavLink to="/profile" style={isActive => ({
+                    color: isActive ? "green" : "blue"
+                })}
+                >Profile</NavLink>
             </div>
-            <div className='item'>
-                <a href="/dialogs">Messages</a>
+            <div className={classes.item}>
+                <NavLink to="/dialogs" style={isActive => ({
+                    color: isActive ? "green" : "blue"
+                })}
+                >Messages</NavLink>
             </div>
-            <div className='item'>
-                <a href="/news">News</a>
+            <div className={classes.item}>
+                <NavLink to="/news" style={isActive => ({
+                    color: isActive ? "green" : "blue"
+                })}>News</NavLink>
             </div>
-            <div className='item'>
-                <a href="/music">Music</a>
+            <div className={classes.item}>
+                <NavLink to="/music" style={isActive => ({
+                    color: isActive ? "green" : "blue"
+                })}>Music</NavLink>
             </div>
-            <div className='item'>
-                <a href="/settings">Settings</a>
+            <div className={classes.item}>
+                <NavLink to="/settings" style={isActive => ({
+                    color: isActive ? "green" : "blue"
+                })}>Settings</NavLink>
             </div>
         </nav>
     )
