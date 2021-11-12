0b5208357df00317d2b0ef5c0cdb54996063825e
nastassia <anastasiyamihalenko@gmail.com>
2021-11-12 13:25:58 +0300

    route - navlink dialogs 22video

--- src/components/dialogs/Dialogs.tsx
+++ src/components/dialogs/Dialogs.tsx
@@ -1,24 +1,30 @@
 import React from "react";
 import classes from "./dialogs.module.css"
+import {NavLink} from "react-router-dom";
 
 const Dialogs = () => {
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogsItem}>
                 <div className={classes.dialog}>
-                    Andrey
+                    <NavLink to="/dialogs/1"
+                    >Andrey</NavLink>
                 </div>
                 <div className={classes.dialog}>
-                    Masha
+                    <NavLink to="/dialogs/2"
+                    >Masha</NavLink>
                 </div>
                 <div className={classes.dialog}>
-                    Sasha
+                    <NavLink to="/dialogs/3"
+                    >Sasha</NavLink>
                 </div>
                 <div className={classes.dialog}>
-                    Timur
+                    <NavLink to="/dialogs/4"
+                    >Timur</NavLink>
                 </div>
                 <div className={classes.dialog}>
-                    Valera
+                    <NavLink to="/dialogs/5"
+                    >Valera</NavLink>
                 </div>
             </div>
             <div className={classes.messages}>
