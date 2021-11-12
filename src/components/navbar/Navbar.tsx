660ad644009451dc899c6afc083cf9660c7c4342
nastassia <anastasiyamihalenko@gmail.com>
2021-11-12 14:26:11 +0300

    active link style

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,35 +2,37 @@ import React from "react";
 import {NavLink} from "react-router-dom";
 import classes from "./navbar.module.css"
 
+type NavNarLinkType = {
+    linkTo: string
+    linkName: string
+}
+
+const NavNarLink = (props: NavNarLinkType) => {
+    return (
+    <div className={classes.item}>
+        <NavLink to={props.linkTo} className={(NavNarLinkType) => NavNarLinkType.isActive ? classes.active : ''}
+        >{props.linkName}</NavLink>
+    </div>
+    )
+}
+
 const Navbar = () => {
     return (
         <nav className={classes.nav}>
             <div className={classes.item}>
-                <NavLink to="/profile" style={isActive => ({
-                    color: isActive ? "green" : "blue"
-                })}
-                >Profile</NavLink>
+                <NavNarLink linkTo={"/profile"} linkName={"Profile"} />
             </div>
             <div className={classes.item}>
-                <NavLink to="/dialogs" style={isActive => ({
-                    color: isActive ? "green" : "blue"
-                })}
-                >Messages</NavLink>
+                <NavNarLink linkTo={"/dialogs"} linkName={"Messages"} />
             </div>
             <div className={classes.item}>
-                <NavLink to="/news" style={isActive => ({
-                    color: isActive ? "green" : "blue"
-                })}>News</NavLink>
+                <NavNarLink linkTo={"/news"} linkName={"News"} />
             </div>
             <div className={classes.item}>
-                <NavLink to="/music" style={isActive => ({
-                    color: isActive ? "green" : "blue"
-                })}>Music</NavLink>
+                <NavNarLink linkTo={"/music"} linkName={"Music"} />
             </div>
             <div className={classes.item}>
-                <NavLink to="/settings" style={isActive => ({
-                    color: isActive ? "green" : "blue"
-                })}>Settings</NavLink>
+                <NavNarLink linkTo={"/settings"} linkName={"Settings"} />
             </div>
         </nav>
     )
