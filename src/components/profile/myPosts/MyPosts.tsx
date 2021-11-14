8265cd2d8ce71dc28a82020da838e1cbf279fc98
nastassia <anastasiyamihalenko@gmail.com>
2021-11-15 00:26:34 +0300

    add styles posts

--- src/components/profile/myPosts/MyPosts.tsx
+++ src/components/profile/myPosts/MyPosts.tsx
@@ -1,14 +1,36 @@
 import React from "react";
 import Post from "./post/Post";
 import classes from "./myPosts.module.css";
+import AddBtn from "../../../assets/add.png"
+import Paperclip from "../../../assets/paperclip.png"
+import Group from "../../../assets/Group.png"
+import Image from "../../../assets/image.png"
+import PostAvatar from "../../../assets/PhotoGirl.png"
+
+export type ButtonType = {
+    bgImg: string
+    title: string
+}
+
+export const Button = (props: ButtonType) => {
+    return <button className={classes.addBtn}><img src={props.bgImg} alt={props.title}/></button>
+}
 
 const MyPosts = () => {
     return (
         <>
-            <textarea className={classes.postTextarea} placeholder={"Type your post"} rows={3}/>
-            <button>Add post</button>
-            <Post message={"Hello"} likeCount={null}/>
-            <Post message={"How are you?"} likeCount={7}/>
+            <div className={classes.wrapper}>
+                <textarea className={classes.postTextarea} placeholder={"Type your post"} rows={3}/>
+                <div className={classes.blockBtns}>
+                    <Button bgImg={AddBtn} title={'#'}/>
+                    <Button bgImg={Paperclip} title={'#'}/>
+                    <Button bgImg={Group} title={'#'}/>
+                    <Button bgImg={Image} title={'#'}/>
+                </div>
+            </div>
+            <Post name ={"Helen"} avatar={PostAvatar} message={"What did the Dursleys care if Harry lost his place on the House Quidditch team because he hadn’t practiced all summer? ."} likeCount={null}/>
+            <Post name ={"Helen"} avatar={PostAvatar} message={"Hello?"} likeCount={7}/>
+
         </>
     )
 }
--- src/components/profile/myPosts/post/Post.tsx
+++ src/components/profile/myPosts/post/Post.tsx
@@ -2,18 +2,25 @@ import React from "react";
 import classes from "./post.module.css"
 
 type PostPropsType = {
+    name: string
+    avatar: string
     message: string
     likeCount: null | number
 };
 
 const Post = (props: PostPropsType) => {
     return (
-        <div className={classes.post}>
-            <img src='https://vk-wiki.ru/wp-content/uploads/2019/06/user-1.png' alt='#'/>
+        <div className={classes.postWrapper}>
+            <div className={classes.postAva}>
+                <img src={props.avatar} alt='#'/>
+                <p className={classes.name}>{props.name}</p>
+            </div >
+            <div className={classes.message}>
             {props.message}
-            <div>
-                <span>like </span>
-                <span>{props.likeCount}</span>
+            </div>
+            <div className={classes.like}>
+                <span>Like</span>
+                <span className={classes.likeCount}>{props.likeCount}</span>
             </div>
         </div>
     )
