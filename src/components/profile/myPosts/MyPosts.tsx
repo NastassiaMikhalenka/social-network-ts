aec2ac24c7bc04d4536a782d727a6c23994ebc69
nastassia <anastasiyamihalenko@gmail.com>
2021-11-14 17:02:56 +0300

    add images

--- src/components/profile/myPosts/MyPosts.tsx
+++ src/components/profile/myPosts/MyPosts.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Post from "./post/Post";
+import classes from "./myPosts.module.css";
 
 const MyPosts = () => {
     return (
         <>
-            <textarea />
+            <textarea className={classes.postTextarea} placeholder={"Type your post"} rows={3}/>
             <button>Add post</button>
             <Post message={"Hello"} likeCount={null}/>
             <Post message={"How are you?"} likeCount={7}/>
