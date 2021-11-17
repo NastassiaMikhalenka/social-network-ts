78d3d6f468faa296e71ad16ca0b397108bd0350c
nastassia <anastasiyamihalenko@gmail.com>
2021-11-17 17:46:32 +0300

    fix link dialogs

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -20,7 +20,7 @@ const Navbar = () => {
     return (
         <nav className={classes.nav}>
             <NavBarLink linkTo={"/profile"} linkName={"Profile"} />
-            <NavBarLink linkTo={"/dialogs"} linkName={"Messages"} />
+            <NavBarLink linkTo={"/dialogs/*"} linkName={"Messages"} />
             <NavBarLink linkTo={"/news"} linkName={"News"} />
             <NavBarLink linkTo={"/music"} linkName={"Music"} />
             <NavBarLink linkTo={"/settings"} linkName={"Settings"} />
