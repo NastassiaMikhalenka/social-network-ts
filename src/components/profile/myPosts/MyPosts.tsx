4e4525a1ab2cc2bef11146fc39a9de12456d499a
nastassia <anastasiyamihalenko@gmail.com>
2021-11-26 23:47:33 +0300

    add

--- src/components/profile/myPosts/MyPosts.tsx
+++ src/components/profile/myPosts/MyPosts.tsx
@@ -1,15 +1,16 @@
-import React from "react";
+import React, {RefObject} from "react";
 import Post from "./post/Post";
 import classes from "./myPosts.module.css";
 import AddBtn from "../../../assets/add.png"
-import Paperclip from "../../../assets/paperclip.png"
-import Group from "../../../assets/Group.png"
-import Image from "../../../assets/image.png"
+// import Paperclip from "../../../assets/paperclip.png"
+// import Group from "../../../assets/Group.png"
+// import Image from "../../../assets/image.png"
 import {PostsDataType} from "../../../redux/state";
 // import PostAvatar from "../../../assets/PhotoGirl.png"
 
 type PropsType = {
     postsData: Array<PostsDataType>
+
 }
 
 const MyPosts = (props: PropsType) => { // принимаем в пропсах postsData и сделали типизацию как PropsType, переходим на уроверь ниже в Post
@@ -20,15 +21,24 @@ const MyPosts = (props: PropsType) => { // принимаем в пропсах 
             name={post.name}
             message={post.message}
             likeCount={post.likeCount}/>)
+
+    let newPostElement: RefObject<HTMLTextAreaElement> = React.createRef();
+
+    const addPost = () => {
+        let text = newPostElement.current?.value
+        alert(text)
+    }
+
     return (
         <>
             <div className={classes.wrapper}>
-                <textarea className={classes.postTextarea} placeholder={"Type your post"} rows={3}/>
+                <textarea ref={newPostElement} className={classes.postTextarea} placeholder={"Type your post"} rows={3}/>
                 <div className={classes.blockBtns}>
-                    <Button bgImg={AddBtn} title={'#'}/>
-                    <Button bgImg={Paperclip} title={'#'}/>
-                    <Button bgImg={Group} title={'#'}/>
-                    <Button bgImg={Image} title={'#'}/>
+                    <button onClick={addPost} className={classes.addBtn}><img src={AddBtn} alt="#"/></button>
+                    {/*<Button bgImg={AddBtn} title={'#'}/>*/}
+                    {/*<Button bgImg={Paperclip} title={'#'}/>*/}
+                    {/*<Button bgImg={Group} title={'#'}/>*/}
+                    {/*<Button bgImg={Image} title={'#'}/>*/}
                 </div>
             </div>
             {postsElements}
@@ -40,11 +50,11 @@ const MyPosts = (props: PropsType) => { // принимаем в пропсах 
 
 export default MyPosts;
 
-type ButtonType = {
-    bgImg: string
-    title: string
-}
-
-const Button = (props: ButtonType) => {
-    return <button className={classes.addBtn}><img src={props.bgImg} alt={props.title}/></button>
-}
+// type ButtonType = {
+//     bgImg: string
+//     title: string
+// }
+//
+// const Button = (props: ButtonType) => {
+//     return <button className={classes.addBtn}><img src={props.bgImg} alt={props.title}/></button>
+// }
